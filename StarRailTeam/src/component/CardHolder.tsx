96e14bb9fa7f3813ac9e8cc6037841fd07429cc7
Brandon Araujo <brandonmtt125@hotmail.com>
2023-09-28 16:06:45 -0400

    Commit

---
+++ StarRailTeam/src/component/CardHolder.tsx
@@ -0,0 +1,11 @@
+import '../css/CardHolder.css'
+
+function CardHolder({teamSize} : {teamSize: number}) {
+
+  return (
+    <div className='CardHolder'>
+    </div>
+  )
+}
+
+export default CardHolder
