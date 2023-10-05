f23ccca9297ab691f73d640c9339208c5e7ec6e5
Brandon Araujo <brandonmtt125@hotmail.com>
2023-10-05 15:29:15 -0400

    Working Commit

--- starRailTeam/src/app/card-holder/card-holder.component.ts
+++ starRailTeam/src/app/card-holder/card-holder.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { FirestoreService } from '../firestore.service';
 import { characterType } from '../Models/character.model';
 
@@ -8,29 +7,23 @@ import { characterType } from '../Models/character.model';
 @Component({
   selector: 'app-card-holder',
   templateUrl: './card-holder.component.html',
-  styleUrls: ['./card-holder.component.scss']
+  styleUrls: ['./card-holder.component.scss'],
 })
 export class CardHolderComponent implements OnInit{
-  selectedCharacterList$!: Observable<characterType[]>;
+  selectedCharacterList$!: characterType[];
   
   constructor(private dataService: DataService, private fireStoreService: FirestoreService){
-  }
-  private _behaviorCharacter: BehaviorSubject<characterType[]> = new BehaviorSubject<characterType[]>([]);
-  get behaviorCharacter$(): Observable<characterType[]>{
-    return this._behaviorCharacter.asObservable()
+    
   }
   
   ngOnInit(): void {
-    this.selectedCharacterList$ = this.dataService.selectedCharactersData$;
-    this.selectedCharacterList$.subscribe(data => {
-      console.log(data)
-    })
     this.fireStoreService.characterData$.subscribe(data => {
-      this._behaviorCharacter.next(data);
+      this.selectedCharacterList$ = data;
     })
-    
   }
   testClick(){
     this.dataService.AddCharacter("Silver Wolf");
+    console.log(this.dataService.selectedCharacters);
+    console.log(this.dataService.selectedCharactersData$);
   }
 }
--- starRailTeam/src/app/data.service.ts
+++ starRailTeam/src/app/data.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, OnInit } from '@angular/core';
-import { Observable, BehaviorSubject, filter, map, take } from 'rxjs';
 import { characterType } from './Models/character.model';
 import { FirestoreService } from './firestore.service';
 
@@ -10,32 +9,31 @@ import { FirestoreService } from './firestore.service';
 //TODO Make two different services for characterData and selectedData
 export class DataService {
 
-  selectedCharactersData$!: Observable<characterType[]>;
+  selectedCharactersData$!: characterType[];
   MAXTEAMSIZE: number = 4;
+  selectedCharacters: string[] = ["Kafka", "Silver Wolf"]
 
   constructor(private fireStoreService: FirestoreService) {
 
-    this._selectedCharacters.subscribe(data => {
-      console.log(data)
-      this.selectedCharactersData$ = this.fireStoreService.characterData$
-        .pipe(map(arr => (data.map(name => (arr.find(character => character.Name === name)))).filter((character): character is characterType => !!character)))
-    })
+    // this._selectedCharacters.subscribe(data => {
+    //   console.log(data)
+    //   this.selectedCharactersData$ = this.fireStoreService.characterData$
+    //     .pipe(map(arr => (data.map(name => (arr.find(character => character.Name === name)))).filter((character): character is characterType => !!character)))
+    // })
 
 
-  }
-  //                                                                        WHY IS THIS PRINTING INIT BUT NOT AFTER UPDATES
-  private _selectedCharacters: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(["Kafka", "Silver Wolf"])
-  get selectedCharacters$(): Observable<string[]> {
-    return this._selectedCharacters.asObservable();
   }
 
   AddCharacter(characterName: string): void {
-    const currentCharacters = this._selectedCharacters.value;
-    if (currentCharacters.length < this.MAXTEAMSIZE) {
-        this._selectedCharacters.next([...currentCharacters, characterName]);
-    } else {
-        console.log("Ran out of team space.");
-    }
+    this.selectedCharacters.push(characterName);
+
+
+    // const currentCharacters = this._selectedCharacters.value;
+    // if (currentCharacters.length < this.MAXTEAMSIZE) {
+    //     this._selectedCharacters.next([...currentCharacters, characterName]);
+    // } else {
+    //     console.log("Ran out of team space.");
+    // }
 }
 
 }
