67c56e596d5e12f43de23d02eb8bbb458a4c37ce
Brandon Araujo <brandonmtt125@hotmail.com>
2023-10-31 12:44:08 -0400

    Filtered character

--- starRailTeam/src/app/sidebar/sidebar.component.ts
+++ starRailTeam/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Output } from '@angular/core';
 import { characterType } from '../Models/character.model';
 import { FirestoreService } from '../services/firestore.service';
 import { DataService } from '../services/data.service';
@@ -10,23 +10,41 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit, OnDestroy{
-  characterList!: characterType[];
   sidebarOpen: boolean = true;
-
+  input: string = '';
+  
   displayedCharacterSub!: Subscription;
   allCharactersSub!: Subscription;
+  
+  characterList!: characterType[];
   displayedCharacters!: characterType[];
+  filteredCharacters!: characterType[];
+
   constructor(
     private firestoreService: FirestoreService,
     private dataService: DataService,
   ) {}
 
+  filterList(e: Event): void{
+    const input = (e.target as HTMLInputElement).value.toLowerCase();
+    this.input = input;
+    this.filteredCharacters = this.characterList.filter(data => (
+      data.Name.toLowerCase().startsWith(input)
+    ))
+  }
+
+  emptyInput(): void{
+    this.input = '';
+    this.filteredCharacters = this.characterList;
+
+  }
   getCharacterInArray(character: string): number{
     return this.displayedCharacters.findIndex(data => data.Name == character) + 1
   }
 
   toggleSidebar() {
     this.sidebarOpen = this.sidebarOpen ? false : true;
+    this.emptyInput()
   }
   toggleCharacter(name: string) {
     if (
@@ -40,6 +58,7 @@ export class SidebarComponent implements OnInit, OnDestroy{
   ngOnInit(): void {
     this.allCharactersSub = this.firestoreService.characterData$.subscribe((data) => {
       this.characterList = data;
+      this.filteredCharacters = data;
     });
 
     this.displayedCharacterSub = this.dataService.displayedCharacters$.subscribe(data => {
