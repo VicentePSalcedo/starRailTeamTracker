93c66a084b9d561781a97130a876d59292f19032
Vicente Salcedo <vicentepsalcedo@gmail.com>
2023-11-09 14:44:10 -0500

    replaced rest script with deployment script that tests first

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Running tests ..."
+if !(firebase emulators:exec --only firestore "cd firestoretests && npm test"); then
+    echo "One or more firestore unit tests are failing."
+    return 1
+fi
+if !(firebase deploy --only firestore); then
+    echo "Firestore rules failed to deploy."
+fi
+echo "Firestore rules successfully deployed."
+if !(firebase deploy --only functions); then
+    echo "Firebase functions failed to deploy."
+fi
+echo "Firebase functions successfully deployed."
