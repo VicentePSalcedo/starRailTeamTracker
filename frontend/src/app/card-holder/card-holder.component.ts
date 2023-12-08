a4a0b1eeb01778b2ebcb16aa6106272bcc6c5529
Vicente Salcedo <vicentepsalced@gmail.com>
2023-12-08 17:48:48 -0500

    round 2... fight

---
+++ frontend/src/app/card-holder/card-holder.component.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../services/firestore.service';
+import { characterType } from '../Models/character.model';
+import { DataService } from '../services/data.service';
+
+
+@Component({
+  selector: 'app-card-holder',
+  templateUrl: './card-holder.component.html',
+  styleUrls: ['./card-holder.component.scss'],
+})
+export class CardHolderComponent implements OnInit {
+  displayedCharacters: characterType[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  getCharacterLength(): number{
+    return this.displayedCharacters.length
+  }
+
+  ngOnInit(): void {
+    this.dataService.displayedCharacters$.subscribe(data => {
+      this.displayedCharacters = data;
+    });
+  }
+}
---
+++ frontend/src/app/services/firestore.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, inject } from '@angular/core';
+import { characterType } from '../Models/character.model';
+import { Firestore, collectionData, collection, getDoc, doc, addDoc, setDoc, DocumentData, docData} from '@angular/fire/firestore';
+import { BehaviorSubject, Observable, Subscription, take } from 'rxjs';
+import { TeamsService } from './teams.service';
+import { User } from 'firebase/auth';
+import { UserAuthService } from './user-auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FirestoreService {
+  private _characterData: BehaviorSubject<characterType[]> =
+    new BehaviorSubject<characterType[]>([]);
+  characterData$: Observable<characterType[]> =
+    this._characterData.asObservable();
+  private _fireStore: Firestore = inject(Firestore);
+
+  user!: User | null
+
+  constructor() {
+    const collectionRef = collection(this._fireStore, 'Characters');
+    collectionData(collectionRef).subscribe((data) => {
+      this._characterData.next(data as characterType[]);
+      // console.log(data)
+    });
+  }
+
+
+  saveTeamToDB(path: string, teamData: characterType[][]): void{
+    teamData.forEach((team, index) => {
+      setDoc(doc(this._fireStore, path, `team${index}`), {...team})
+    })
+  }
+  writeDoc<T>(path: string, data: {}){
+    setDoc(doc(this._fireStore, path), data)
+  }
+  getDoc(path: string): Observable<DocumentData>{
+    return docData(doc(this._fireStore, path))
+  }
+  getCollectionSubscription(path: string): Observable<DocumentData>{
+    const collectionRef = collection(this._fireStore, path)
+    return (collectionData(collectionRef))
+  }
+
+}
