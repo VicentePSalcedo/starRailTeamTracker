9cfef21def93b1c247bbab9280bd81774a928518
Vicente Salcedo <vicentepsalced@gmail.com>
2023-12-08 10:24:04 -0500

    updated env variables

--- scrapper/webscrapper_v3/src/main.rs
+++ scrapper/webscrapper_v3/src/main.rs
@@ -6,20 +6,23 @@ use serde::{Deserialize, Serialize};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let db = FirestoreDb::new("starrailteamtrackerdev").await?;
-    const COLLECTION_NAME: &'static str = "test";
+    const COLLECTION_NAME: &'static str = "Characters";
 
     let response = get_html("https://game8.co/games/Honkai-Star-Rail/archives/404256");
-    let tbodies = get_tag(&response.await.as_ref().unwrap(), "tbody");
 
+    let tbodies = get_tag(&response.await.as_ref().unwrap(), "tbody");
     let tbody_fragment = Html::parse_fragment(&tbodies[1]);
     let a_selector = Selector::parse("a").unwrap();
 
     println!("Writing to {:?}...", COLLECTION_NAME);
+
     for (count, element) in tbody_fragment.select(&a_selector).enumerate() {
         if count % 4 == 0 {
             let text: Vec<&str> = element.text().collect();
+
             let name = text[0].trim();
             let character_data = get_html(&element.value().attr("href").unwrap());
+
             let table = get_tag(
                 &character_data.await.as_ref().unwrap(),
                 "div.a-tabContainer",
@@ -27,8 +30,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             let links = get_tag(&table.concat(), "a");
             let main_stats_html = get_tag(&table.concat(), "td");
             let light_cone = get_text(&links[0], "a").trim().to_owned();
+
             let mut relics: Vec<String> = Vec::new();
             let mut ornaments: Vec<String> = Vec::new();
+
             if links.len() == 8 {
                 relics.push(get_text(&links[1], "a").trim().to_owned());
                 ornaments.push(get_text(&links[2], "a").trim().to_owned());
@@ -37,11 +42,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 relics.push(get_text(&links[2], "a").trim().to_owned());
                 ornaments.push(get_text(&links[2], "a").trim().to_owned());
             }
+
             let regex = Regex::new(r#"(<b class=\"a-bold\">)[A-Za-z]*(</b>):*"#).unwrap();
             let preped_main_stats_html = regex.replace_all(&main_stats_html[3], " ");
             let ruf_stats = get_text(&preped_main_stats_html, "html");
             let final_stats: Vec<&str> = ruf_stats.trim().split("  ").collect();
+
             let delims = Regex::new(r"/| or ").unwrap();
+
             let body_main_stat: Vec<String> =
                 delims.split(final_stats[0]).map(str::to_string).collect();
             let feet_main_stat: Vec<String> =
@@ -50,6 +58,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 delims.split(final_stats[2]).map(str::to_string).collect();
             let rope_main_stat: Vec<String> =
                 delims.split(final_stats[3]).map(str::to_string).collect();
+
             let character = Character {
                 Name: name.to_owned(),
                 LightCone: LightCone {
@@ -67,6 +76,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                     Rope: rope_main_stat,
                 },
             };
+
             let _update_builder: Character = db
                 .fluent()
                 .update()
